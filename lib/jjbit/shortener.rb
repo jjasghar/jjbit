d8027d6f16006d7cb9e8f0fccdde865d8270cf16
JJ Asghar <jj@getchef.com>
2015-01-23 15:32:42 -0600

    Shortener talks to bit.ly with testing.

--- lib/jjbit/shortener.rb
+++ lib/jjbit/shortener.rb
@@ -1,3 +1,7 @@
+require "net/https"
+require "cgi"
+require "json"
+
 module Jjbit
   class Shortener
 
@@ -7,7 +11,26 @@ module Jjbit
       @token = token
     end
 
+    def shorten(url)
+      raise NotAUrl unless url =~ URI.regexp
 
+      uri = URI.parse("https://api-ssl.bitly.com/v3/shorten?access_token=#{token}&longUrl=#{escape(url)}")
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      request = Net::HTTP::Get.new(uri.request_uri)
+      response = http.request(request)
+      JSON.parse(response.body)["data"]["url"]
+    end
+
+    private
+
+    def escape(url)
+      CGI.escape(url.to_s)
+    end
 
   end
+
+  class NotAUrl < StandardError
+  end
+
 end
--- spec/shortener_spec.rb
+++ spec/shortener_spec.rb
@@ -3,15 +3,23 @@ require 'spec_helper'
 describe Jjbit::Shortener do
 
   it 'has my_token and the token' do
-    expander = Jjbit::Shortener.new("my_token")
-    expect(expander.token).to eq("my_token")
+    shortener = Jjbit::Shortener.new("my_token")
+    expect(shortener.token).to eq("my_token")
   end
 
   it 'should have another_token as the token' do
-    expander = Jjbit::Shortener.new("another_token")
-    expect(expander.token).to eq("another_token")
+    shortener = Jjbit::Shortener.new("another_token")
+    expect(shortener.token).to eq("another_token")
   end
 
+  it 'return a shorten url and the string' do
+    shortener = Jjbit::Shortener.new("0c984385a6d2462b9d7e1ec370e4251e5eaea9dc")
+    expect(shortener.shorten("http://google.com")).to eq("http://bit.ly/1ra0tQp")
+  end
 
+  it 'should raise not_a_url because its not a url' do
+    shortener = Jjbit::Shortener.new("0c984385a6d2462b9d7e1ec370e4251e5eaea9dc")
+    expect{ shortener.shorten("NOT_A_URL") }.to raise_error(Jjbit::NotAUrl)
+  end
 
 end
