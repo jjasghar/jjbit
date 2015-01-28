e38a1852bcdcb19274b7b4b6e2cdf140ac6ba486
JJ Asghar <jj@getchef.com>
2015-01-27 18:51:01 -0600

    got the expander working

--- lib/jjbit/expander.rb
+++ lib/jjbit/expander.rb
@@ -1,3 +1,8 @@
+require "net/https"
+require "cgi"
+require "json"
+
+
 module Jjbit
   class Expander
 
@@ -7,7 +12,28 @@ module Jjbit
       @token = token
     end
 
+    def expand(url)
+      raise NotAUrl unless url =~ URI.regexp
 
+      uri = URI.parse("https://api-ssl.bitly.com/v3/expand?access_token=#{token}&shortUrl=#{escape(url)}")
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      request = Net::HTTP::Get.new(uri.request_uri)
+      response = http.request(request)
+      JSON.parse(response.body)["data"]["expand"][0]["long_url"]
+    end
+
+
+    private
+
+    def escape(url)
+      CGI.escape(url.to_s)
+    end
 
   end
+
+  class NotAUrl < StandardError
+  end
+
+
 end
--- spec/expander_spec.rb
+++ spec/expander_spec.rb
@@ -12,6 +12,15 @@ describe Jjbit::Expander do
     expect(expander.token).to eq("another_token")
   end
 
+  it 'return a expand the url and the string' do
+    expander = Jjbit::Expander.new("0c984385a6d2462b9d7e1ec370e4251e5eaea9dc")
+    expect(expander.expand("http://bit.ly/1ra0tQp")).to eq("http://google.com/")
+  end
+
+  it 'should raise not_a_url because its not a url' do
+    expander = Jjbit::Expander.new("0c984385a6d2462b9d7e1ec370e4251e5eaea9dc")
+    expect{ expander.expand("NOT_A_URL") }.to raise_error(Jjbit::NotAUrl)
+  end
 
 
 end
